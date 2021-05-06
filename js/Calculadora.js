2490f961aa7747c96fc5c24fad3f7f08570934de
Josiascrack <73666351+Josiascrack@users.noreply.github.com>
2021-05-05 23:00:15 -0500

    CALCULADORA
    TERMINADO

---
+++ js/Calculadora.js
@@ -0,0 +1,26 @@
+class Calculadora {
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+    porcentaje(num1, num2){
+        return num1 % num2;
+    }
+    raiz(num1){
+        return Math.sqrt(num1);
+    }
+    potencia(num1, num2){
+        return Math.pow(num1, num2);
+    }
+} 
